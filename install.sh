326ed899d1c379a37347975b010fe933458487d3
gitondezhuts <156513740+gitondezhuts@users.noreply.github.com>
2024-02-09 21:51:26 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -326,11 +326,9 @@ server {
   server_name ${wanip};
       location / {
         proxy_pass http://127.0.0.1:8000/;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 }
 }
 EOF
